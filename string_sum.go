4c70a9bd5f09c47b2f4078cde2d3b8518b28cde2
admin <admin@admin.net>
2022-05-19 16:33:05 +0300

    add solution

--- string_sum.go
+++ string_sum.go
@@ -2,6 +2,9 @@ package string_sum
 
 import (
 	"errors"
+	"fmt"
+	"strconv"
+	"strings"
 )
 
 //use these errors as appropriate, wrapping them with fmt.Errorf function
@@ -23,5 +26,70 @@ var (
 // Use the errors defined above as described, again wrapping into fmt.Errorf
 
 func StringSum(input string) (output string, err error) {
-	return "", nil
+	number1, number2, err := extractTwoInt(input)
+	if err != nil {
+		return "", fmt.Errorf("%w", err)
+	}
+
+	sum := number1 + number2
+	output = strconv.Itoa(sum)
+
+	return
+}
+
+func extractTwoInt(input string) (firstInt int, secondInt int, err error) {
+	input = strings.Join(strings.Fields(input), "")
+
+	if len(input) < 1 {
+		return firstInt, secondInt, errorEmptyInput
+	}
+
+	operandsAsStrings := extractTwoAssumedOperands(input)
+
+	if len(operandsAsStrings) != 2 {
+		return firstInt, secondInt, errorNotTwoOperands
+	}
+
+	integers, err := parseIntegersFromStrings(operandsAsStrings)
+
+	if err != nil {
+		return firstInt, secondInt, err
+	}
+
+	firstInt = integers[0]
+	secondInt = integers[1]
+
+	return
+}
+
+func extractTwoAssumedOperands(input string) (operands []string) {
+	operands = strings.Split(input, "+")
+	if len(operands) != 2 {
+		operands = strings.Split(input, "-")
+
+		// case "X-Y"
+		if len(operands) == 2 {
+			operands = []string{operands[0], "-" + operands[1]}
+		}
+
+		// case "-X-Y"
+		if len(operands) == 3 {
+			operands = []string{"-" + operands[1], "-" + operands[2]}
+		}
+	}
+
+	return
+}
+
+func parseIntegersFromStrings(input []string) (integers []int, err error) {
+	for _, chunk := range input {
+		foundInt, err := strconv.Atoi(chunk)
+		if err != nil {
+			return integers, err
+		}
+
+		integers = append(integers, foundInt)
+	}
+
+	return
 }
